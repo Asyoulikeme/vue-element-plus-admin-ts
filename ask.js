a5fe1def4b7c618a6b775a01bda3d271d7dee3e0
wuyuwei <wuyuwei2016@gmail.com>
2021-02-10 16:51:34 +0800

    change ask.js

--- ask.js
+++ ask.js
@@ -1,5 +1,17 @@
-module.exports = {
-    name: "",
-    author: "",
-    description: ""
-}
+module.exports = [
+    {
+        type: "input",
+        name: "name",
+        message: "App name:"
+    },
+    {
+        type: "input",
+        name: "author",
+        message: "author:"
+    },
+    {
+        type: "input",
+        name: "author",
+        message: "description:"
+    },
+]
